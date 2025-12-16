6be4855bdca1c051967373f33cf2a90d297cacfe
Gabriel Mota <gom.mota@gmail.com>
2025-12-15 21:53:28 -0300

    fix: ThumbRating stars when image not loaded

--- src/components/ThumbRating/ThumbRating.vue
+++ src/components/ThumbRating/ThumbRating.vue
@@ -5,28 +5,40 @@ const MAX_STARS = 5
 </script>
 
 <template>
-  <QImg :src="imagePath" fit="cover" class="border-radius-inherit" height="100%">
-    <div style="background-color: transparent">
-      <div class="rating">
-        <QIcon
-          v-for="i in MAX_STARS"
-          :key="i"
-          name="star"
-          :color="i <= Number(stars) ? 'secondary' : 'grey-4'"
-          size="14px"
-        />
-      </div>
+  <div class="thumb-rating border-radius-inherit">
+    <QImg
+      :src="imagePath"
+      fit="cover"
+      class="border-radius-inherit"
+      height="100%"
+      loading-show-delay="6000"
+    >
+      <template v-slot:error>
+        <div class="empty-image">
+          <QIcon name="image" size="42px" color="grey-6" />
+          <span class="empty-image__text">Imagem não encontrada</span>
+        </div>
+      </template>
+    </QImg>
+
+    <div class="rating">
+      <QIcon
+        v-for="i in MAX_STARS"
+        :key="i"
+        name="star"
+        :color="i <= Number(stars) ? 'secondary' : 'grey-4'"
+        size="14px"
+      />
     </div>
-    <template v-slot:error>
-      <div class="empty-image">
-        <QIcon name="image" size="42px" color="grey-6" />
-        <span class="empty-image__text">Imagem não encontrada</span>
-      </div>
-    </template>
-  </QImg>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.thumb-rating {
+  position: relative;
+  height: 100%;
+}
+
 .rating {
   display: inline-flex;
   align-items: center;
@@ -35,6 +47,9 @@ const MAX_STARS = 5
   background: white;
   box-shadow: $shadow;
   border-radius: $border-radius-large;
+  position: absolute;
+  top: $spacing-medium;
+  left: $spacing-medium;
 }
 
 .empty-image {
